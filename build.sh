e92fb98f4b72328b87e2c9cfdecb96700dfc5c9c
Nicky Gerritsen <nickygerritsen@me.com>
2013-07-20 17:23:55 +0200

    This will fail the travis build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd "NGSegmentedViewController example"
-~/travis-utils/osx-cibuild.sh
+~/travis-utils/osx-cibuild.sh || exit $?
 cd ..
 cd "NGSegmentedViewController IB example"
-~/travis-utils/osx-cibuild.sh
+~/travis-utils/osx-cibuild.sh || exit $?
 cd ..
