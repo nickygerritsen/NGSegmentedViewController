3398a033afa1d2221b67b95daae45e18151eec16
Nicky Gerritsen <nicky@andare.com>
2013-08-26 11:41:00 +0200

    Use xctool for compiling

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 cd "NGSegmentedViewController example"
-~/travis-utils/osx-cibuild.sh || exit $?
+pod update
+xctool -workspace NGSegmentedViewController\ example.xcworkspace -scheme "NGSegmentedViewController example" -configuration Release -sdk iphonesimulator -arch i386 build || exit $?
 cd ..
 cd "NGSegmentedViewController IB example"
-~/travis-utils/osx-cibuild.sh || exit $?
+pod update
+xctool -workspace NGSegmentedViewController\ IB\ example.xcworkspace -scheme "NGSegmentedViewController IB example" -configuration Release -sdk iphonesimulator -arch i386 build || exit $?
 cd ..
